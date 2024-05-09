c055587ee62be157bbccbcf99714ce4d984f3272
Caio César <caiogalaxy00@gmail.com>
2024-05-09 14:20:15 -0300

    Identação

---
+++ java-anatomia-classes/src/BoletimEstudantil.java
@@ -0,0 +1,12 @@
+public class BoletimEstudantil {
+    public static void main(String[] args) {
+        int medialFinal = 7;
+
+        if (medialFinal < 6)
+            System.out.println("REPROVADO");
+        else if (medialFinal == 6)
+            System.out.println("PROVA MINERVA");
+        else
+            System.out.println("APROVADO");
+    }
+}
--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,5 +1,15 @@
 public class MinhaClasse {
+
     public static void main (String [] args) {
-        System.out.print ("Olá turma, sejma bem-vindos!");
+        String primeiroNome = "Caio";
+        String segundoNome = "César";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
     }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+
 }
