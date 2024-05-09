930452a8f2335a77a6fe30272004b7465c21ce28
Caio César <caiogalaxy00@gmail.com>
2024-05-09 15:29:30 -0300

    Tipos e Variáveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,3 +1,20 @@
 public class TiposVariaveis {
-    
+    public static void main (String [] args) throws Exception {
+        double salarioMinimo = 3680.55;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;  // casting
+
+        int numero = 1;
+
+        numero = 2;
+
+        final double VALOR_DE_PI = 3.14;
+
+        String meuNome = "CAIO CÉSAR";
+
+        System.out.print(VALOR_DE_PI);
+        
+    }
 }
