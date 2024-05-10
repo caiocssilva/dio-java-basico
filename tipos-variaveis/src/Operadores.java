8704e5934f85ed42697a68539e8776b07a26822a
Caio César <caiogalaxy00@gmail.com>
2024-05-10 09:34:18 -0300

    Concatenação

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,41 @@
+public class Operadores {
+    public static void main (String [] args) {
+        /* 
+        //classe Operadores.java
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+		
+        //qual o resultado das expressoes abaixo?
+        String concatenacao ="?"; 
+
+        concatenacao = 1+1+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+
+        System.out.println(concatenacao);
+        */
+
+        int numero = 5;
+
+        numero = - numero;
+
+        System.out.println(numero);
+
+        numero = + numero * -1; 
+
+        System.out.println(numero);
+    }
+}
