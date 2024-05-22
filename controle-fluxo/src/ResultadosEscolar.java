6d7919687cc5a21f70417222ea163825ca8a530b
Caio César <caiogalaxy00@gmail.com>
2024-05-22 11:33:21 -0300

    Condição ternaria

--- controle-fluxo/src/ResultadosEscolar.java
+++ controle-fluxo/src/ResultadosEscolar.java
@@ -1,11 +1,25 @@
 public class ResultadosEscolar {
     public static void main(String[] args) {
-        int nota = 8;
+        /*int nota = 6;
 
         if(nota >= 7)
             System.err.println("APROVADO");
 
+        else if (nota >= 5 && nota < 7)
+            System.err.println("RECUPERAÇÃO");
+
         else
             System.err.println("REPROVADO");
+        
+
+        int nota = 6;
+        String resultado = nota >= 7 ? "APROVADO" : "REPROVADO";
+        System.out.println(resultado);
+        */
+
+        int nota = 4;
+        String resultado = nota >= 7 ? "APROVADO" : nota >= 5 && nota < 7 ? "RECUPERAÇÃO" : "REPROVADO";
+        System.out.println(resultado);
+
     }
 }
