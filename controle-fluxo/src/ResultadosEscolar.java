1099e8fb3fb0332ed538ba242d980d9f47a469f2
Caio César <caiogalaxy00@gmail.com>
2024-05-22 10:46:25 -0300

    Estruturas condicionais

---
+++ controle-fluxo/src/ResultadosEscolar.java
@@ -0,0 +1,11 @@
+public class ResultadosEscolar {
+    public static void main(String[] args) {
+        int nota = 8;
+
+        if(nota >= 7)
+            System.err.println("APROVADO");
+
+        else
+            System.err.println("REPROVADO");
+    }
+}
